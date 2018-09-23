3b1342a88d087bdebc03c01ae1d3954b414163ec
Kevin Tan <kevin.tan97@hotmail.com>
2018-09-23 19:14:01 -0400

    added iterative script execution on db on startup

--- database/src/main/java/com/soen343/project/database/DatabaseConstants.java
+++ database/src/main/java/com/soen343/project/database/DatabaseConstants.java
@@ -8,6 +8,8 @@ public final class DatabaseConstants {
 
     private DatabaseConstants() {}
 
+    public static final String SCRIPT_DIRECTORY =
+            "C:\\Users\\Kevin Tan\\Desktop\\Concordia\\Year 3\\Semester 7\\SOEN343\\Project\\Project343\\database\\src\\main\\resources";
     public final static String DATABASE_PATH =
             "C:\\Users\\Kevin Tan\\Desktop\\Concordia\\Year 3\\Semester 7\\SOEN343\\Project\\Project343\\data\\LibraryCatalog.db";
 }
--- database/src/main/java/com/soen343/project/database/schema/CreateDatabase.java
+++ database/src/main/java/com/soen343/project/database/schema/CreateDatabase.java
@@ -1,9 +1,16 @@
 package com.soen343.project.database.schema;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import static com.soen343.project.database.DatabaseConstants.*;
 
 /**
  * Created by Kevin Tan 2018-09-23
@@ -12,15 +19,21 @@ import javax.annotation.PostConstruct;
 public class CreateDatabase {
 
     private final boolean createTable;
+    private final List<Resource> dbScripts = new LinkedList<>();
 
     public CreateDatabase(boolean createTable) {
         this.createTable = createTable;
+        File directory = new File(SCRIPT_DIRECTORY);
+        for (File file : Objects.requireNonNull(directory.listFiles())) {
+            dbScripts.add(new ClassPathResource(file.getAbsolutePath()));
+        }
     }
 
     @PostConstruct
     public void createTable() {
+        //Requires connection and Script for resource
         if (createTable) {
-            ScriptUtils.executeSqlScript(null, (Resource) null);
+            dbScripts.forEach(resource -> ScriptUtils.executeSqlScript(null, resource));
         }
     }
 
