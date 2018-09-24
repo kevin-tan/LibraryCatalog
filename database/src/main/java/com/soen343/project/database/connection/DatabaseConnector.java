86e9c30884bf9a78dee7be2999d3bfd193453dc6
Kevin Tan <kevin.tan97@hotmail.com>
2018-09-24 11:53:26 -0400

    renaming Database file

--- database/src/main/java/com/soen343/project/database/connection/DatabaseConnector.java
+++ database/src/main/java/com/soen343/project/database/connection/DatabaseConnector.java
@@ -1,22 +1,25 @@
 package com.soen343.project.database.connection;
 
-        import java.sql.Connection;
-        import java.sql.DriverManager;
-        import java.sql.SQLException;
-        import java.sql.Statement;
+import com.soen343.project.database.DatabaseConstants;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
- * Created by Kevin Tan 2018-09-23
+ * Created by Sagar Patel 2018-09-23
  */
+
 public class DatabaseConnector {
 
     public static void executeQuery(String query) throws ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
 
         //try-with-resource
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:LibraryCatalog.db")) {
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DatabaseConstants.DATABASE_PATH)) {
             Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
+            statement.setQueryTimeout(30); // Time-out if database does not execute any queries in 30 seconds
             statement.executeQuery(query);
         } catch (SQLException e) {
             e.printStackTrace();
