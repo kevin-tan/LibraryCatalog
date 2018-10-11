28d6a1013c7c9edf8e37bf7ddc0dae520e301236
Luca Fiorilli <luca30@live.ca>
2018-10-11 14:46:05 -0400

    ISSUE31: Update implementation of UserRegistry

---
+++ service/src/main/java/com/soen343/project/service/authenticate/LoginManager.java
@@ -0,0 +1,57 @@
+package com.soen343.project.service.authenticate;
+
+import com.soen343.project.repository.entity.user.User;
+import com.soen343.project.service.database.RecordDatabase;
+import com.soen343.project.service.notification.Observer;
+import com.soen343.project.service.notification.Subject;
+import com.soen343.project.service.registry.UserRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class LoginManager implements Subject {
+
+    private RecordDatabase recordDatabase;
+    private List<Observer> observers;
+
+    @Autowired
+    LoginManager(UserRegistry userRegistry, RecordDatabase recordDatabase) {
+        this.recordDatabase = recordDatabase;
+        observers = new LinkedList<>();
+        addObserver(userRegistry);
+    }
+
+    //TODO: Change to String parameter insted of User
+    public boolean loginUser(User username){
+        // must be called after successful login
+        notifyObservers(username);
+        return true;
+    }
+
+    //TODO: Change to String parameter insted of User
+    public boolean logoutUser(User username) {
+        // must be called after successful logout
+        notifyObservers(username);
+        return true;
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        this.observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        this.observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers(Object data) {
+        for (Observer observer: observers) {
+            observer.update(data);
+        }
+    }
+}
--- service/src/main/java/com/soen343/project/service/registry/UserRegistry.java
+++ service/src/main/java/com/soen343/project/service/registry/UserRegistry.java
@@ -4,6 +4,7 @@ import com.soen343.project.repository.instance.ActiveUser;
 import com.soen343.project.repository.entity.user.User;
 import com.soen343.project.service.authenticate.AuthenticationService;
 import com.soen343.project.service.database.RecordDatabase;
+import com.soen343.project.service.notification.Observer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 @Service
-public class UserRegistry {
+public class UserRegistry implements Observer {
 
     private final RecordDatabase recordDatabase;
     private final AuthenticationService authenticationService;
@@ -28,7 +29,6 @@ public class UserRegistry {
     //TODO:This gets all users for now. We need login feature to view active users.
     public List<ActiveUser> viewActiveUserRegistry(Long id) {
         if (this.authenticationService.authenticateAdmin(id)) {
-            recordDatabase.findAllUsers().forEach(this::addActiveUser);
             return activeUsers;
         }
         return null;
@@ -44,5 +44,35 @@ public class UserRegistry {
 
         ActiveUser activeUser = new ActiveUser(user.getId());
         activeUsers.add(activeUser);
+    }
+
+    private void removeActiveUser(User user) {
+
+        for (ActiveUser activeUser : activeUsers) {
+            if (activeUser.getId().equals(user.getId())) {
+                activeUsers.remove(activeUser);
+                return;
+            }
+        }
+    }
+
+    private boolean isActiveUser(User user) {
+
+        for (ActiveUser activeUser : activeUsers) {
+            if (activeUser.getId().equals(user.getId())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void update(Object data) {
+        if (isActiveUser((User) data)) {
+            removeActiveUser((User) data);
+        } else {
+            addActiveUser((User) data);
+        }
     }
 }
