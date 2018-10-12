d90862e6fc2ced6ee051ae5d9087bc9e8780480c
JeffreyLi16 <jeffreyli16@hotmail.com>
2018-10-12 00:52:40 -0400

    Register form data submittion
    #28

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from "./user";
+import { RegisterService } from './register.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui';
+
+  userModel = new User('jeff', 'jeff', 'Jeffrey','Li', 'jeffreyli16@hotmail.com', '5145829225', 'test');
+
+  constructor(private _registerService: RegisterService){}
+
+  onSubmit() {
+    this._registerService.register(this.userModel)
+      .subscribe(
+        data => console.log('Success!', data),
+        error => console.log('Error!', error)
+      )
+  }
 }
