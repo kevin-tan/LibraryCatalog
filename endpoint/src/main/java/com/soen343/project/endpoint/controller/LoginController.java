a4fdeee00abc0c2843027877d35e05b51c9b7d79
StephiieC <stephanie.chiang@hotmail.com>
2018-10-12 01:48:24 -0400

    linking backend to frontend (fail)

--- endpoint/src/main/java/com/soen343/project/endpoint/controller/LoginController.java
+++ endpoint/src/main/java/com/soen343/project/endpoint/controller/LoginController.java
@@ -5,8 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/app/v1")
 public class LoginController {
 
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ui';
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string){
+
+    let headers = new HttpHeaders();
+    headers = headers.append("Authorization", "Basic " + btoa("username:password"));
+    headers = headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    this.http.post('http://localhost:8080/app/v1/login ',"", {headers: headers}).subscribe(response => {
+      console.log(response);
+    }, err => {
+      console.log("User authentication failed!");
+    })
+  }
+
 }
