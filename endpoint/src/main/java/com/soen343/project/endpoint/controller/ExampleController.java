8bbc3edf5a78d52cb9f1534717220675d50ccd9e
Neqqash Hussin <neqqash96@gmail.com>
2018-10-11 21:39:41 -0400

    File was missing password attribute for builder
    Co-Authored-By: tomminator97 <tomminator97@users.noreply.github.com>

--- endpoint/src/main/java/com/soen343/project/endpoint/controller/ExampleController.java
+++ endpoint/src/main/java/com/soen343/project/endpoint/controller/ExampleController.java
@@ -48,7 +48,7 @@ public class ExampleController {
     @GetMapping("/test/addNewUser")
     public ResponseEntity<?> addNewUser() {
         Admin admin = Admin.builder().firstName("Test First").lastName("Test Last").email("Test@hotmail.com").phoneNumber("514-Test")
-                .physicalAddress("888 Test").build();
+                .physicalAddress("888 Test").password("bigboss").build();
         userRepository.save(admin);
         return new ResponseEntity<>(userRepository.findById(2L), HttpStatus.OK);
     }
@@ -60,9 +60,9 @@ public class ExampleController {
     @GetMapping("/test/populateUsers")
     public ResponseEntity<?> populateUsers() {
         Admin admin = Admin.builder().firstName("Test First").lastName("Test Last").email("Test@hotmail.com").phoneNumber("514-Test")
-                .physicalAddress("888 Test").build();
+                .physicalAddress("888 Test").password("bigboss").build();
         Client client = Client.builder().firstName("John").lastName("Wick").email("john@wick.com").phoneNumber("1234567")
-                .physicalAddress("John Wicks House").build();
+                .physicalAddress("John Wicks House").password("johnwicksdog").build();
         userRepository.saveAll(admin, client);
         return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
     }
@@ -75,7 +75,7 @@ public class ExampleController {
         User oldAdmin = userRepository.findById(id);
         Admin admin =
                 Admin.builder().firstName("Test First2").lastName("Test Last2").email("Tes2t@hotmail.com32").phoneNumber("51324-Tes32t")
-                        .physicalAddress("88832 Test32").id(oldAdmin.getId()).build();
+                        .physicalAddress("88832 Test32").password("testtest").id(oldAdmin.getId()).build();
         userRepository.update(admin);
         return new ResponseEntity<>(userRepository.findById(oldAdmin.getId()), HttpStatus.OK);
     }
