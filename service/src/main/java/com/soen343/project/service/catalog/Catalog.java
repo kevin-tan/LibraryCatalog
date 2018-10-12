ddefb0561b7f0760386581ffc1e5fed0a4cac5dc
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2018-10-11 22:54:31 -0400

    Created the Catalog service

---
+++ service/src/main/java/com/soen343/project/service/catalog/Catalog.java
@@ -0,0 +1,38 @@
+package com.soen343.project.service.catalog;
+
+import com.soen343.project.repository.entity.catalog.ItemSpecification;
+import com.soen343.project.service.database.RecordDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class Catalog {
+
+    private final RecordDatabase recordDatabase;
+
+    @Autowired
+    public Catalog(RecordDatabase recordDatabase){
+        this.recordDatabase = recordDatabase;
+    }
+
+    public List editItem(long itemID, ItemSpecification itemSpec){
+        recordDatabase.updateItem(itemID, itemSpec);
+        return getAllItem();
+    }
+
+    public List deleteCatalogItem(long itemID){
+        recordDatabase.removeItem(itemID);
+        return getAllItem();
+    }
+
+    public List addCatalogItem(ItemSpecification itemSpec){
+        recordDatabase.insertCatalogItem(itemSpec);
+        return getAllItem();
+    }
+
+    public List getAllItem(){
+        return recordDatabase.findAllItem();
+    }
+}
