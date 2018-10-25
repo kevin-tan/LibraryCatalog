888e40d2b78169d5fc59d7e48bb447f9e539740b
StephiieC <stephanie.chiang@hotmail.com>
2018-10-25 12:44:00 -0400

    new files, login redirect

---
+++ ui/src/app/login/login-redirect.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from "@angular/router";
+
+@Injectable()
+export class LoginRedirectService implements CanActivate{
+
+  constructor(private router: Router) {}
+  getStatus():boolean{
+    return new Boolean(sessionStorage.getItem('status')).valueOf();
+  }
+  canActivate(): boolean{
+    if(this.getStatus()== true){
+      this.router.navigate(['home']);
+      return false;
+    }else{
+      return true;
+    }
+  }
+}
