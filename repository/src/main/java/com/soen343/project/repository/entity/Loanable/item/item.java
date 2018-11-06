8d3fae9eee3731c78155c9de962375f67432955b
Neqqash Hussin <neqqash96@gmail.com>
2018-11-05 22:44:41 -0500

    Created Loanable item Entities
    Co-Authored-By: tomminator97 <tomminator97@users.noreply.github.com>

--- repository/src/main/java/com/soen343/project/repository/entity/Loanable/item/item.java
+++ repository/src/main/java/com/soen343/project/repository/entity/Loanable/item/item.java
@@ -1,4 +1,72 @@
 package com.soen343.project.repository.entity.Loanable.item;
 
-public class item {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.soen343.project.database.base.DatabaseEntity;
+import com.soen343.project.repository.entity.catalog.itemspec.ItemSpecification;
+import lombok.Data;
+
+import static com.soen343.project.repository.entity.EntityConstants.*;
+
+@Data
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+
+public class item implements DatabaseEntity {
+    private Long id;
+    private ItemSpecification spec;
+    private String type;
+
+
+    public item(Long id, ItemSpecification spec) {
+        this.id = id;
+        this.spec = spec;
+        this.type = spec.getClass().getSimpleName();
+    }
+
+    public item(ItemSpecification spec) {
+        this.spec = spec;
+        this.type = spec.getClass().getSimpleName();
+    }
+
+    public item(Long id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public void setSpec(ItemSpecification spec) {
+        this.spec = spec;
+        this.type = spec.getClass().getSimpleName();
+    }
+
+
+    @Override
+    public String sqlUpdateValues() {
+        String columnValues = ITEMSPECID + " = '" + spec.getId() + "', ";
+        columnValues += TYPE + " = '" + type + "'";
+        return columnValues;
+    }
+
+    @Override
+    @JsonIgnore
+    public String toSQLValue() {
+        return "('" + spec.getId() + "','" + type + "')";
+    }
+
+    @Override
+    @JsonIgnore
+    public String getTable() {
+        return ITEM_TABLE;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getTableWithColumns() {
+        return ITEM_TABLE_WITH_COLUMNS;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
 }
+
--- repository/src/main/java/com/soen343/project/repository/entity/Loanable/itemspec/printed/common/PrintedItem.java
+++ repository/src/main/java/com/soen343/project/repository/entity/Loanable/itemspec/printed/common/PrintedItem.java
@@ -1,4 +1,46 @@
 package com.soen343.project.repository.entity.Loanable.itemspec.printed.common;
 
-public class PrintedItem {
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.soen343.project.repository.entity.catalog.itemspec.ItemSpecification;
+import com.soen343.project.repository.entity.catalog.itemspec.printed.Book;
+import com.soen343.project.repository.entity.catalog.itemspec.printed.Magazine;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@JsonSubTypes({@JsonSubTypes.Type(value = Book.class, name = "Book"), @JsonSubTypes.Type(value = Magazine.class, name = "Magazine")})
+@NoArgsConstructor
+public abstract class PrintedItem extends ItemSpecification {
+
+    private String publisher;
+    private String pubDate;
+    private String language;
+    private String isbn10;
+    private String isbn13;
+
+    public PrintedItem(long id, String title, String publisher, String pubDate,
+                       String language, String isbn10, String isbn13){
+        super(id, title);
+        this.publisher = publisher;
+        this.pubDate = pubDate;
+        this.language = language;
+        this.isbn10 = isbn10;
+        this.isbn13 = isbn13;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PrintedItem)) return false;
+        if (!super.equals(o)) return false;
+        PrintedItem that = (PrintedItem) o;
+        return Objects.equals(publisher, that.publisher) &&
+                Objects.equals(pubDate, that.pubDate) &&
+                Objects.equals(language, that.language) &&
+                Objects.equals(isbn10, that.isbn10) &&
+                Objects.equals(isbn13, that.isbn13);
+    }
 }
+
