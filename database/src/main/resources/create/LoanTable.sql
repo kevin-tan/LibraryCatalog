14646eae31eeee6bf80dd4ae848fd736d919c40a
Neqqash Hussin <neqqash96@gmail.com>
2018-11-05 23:32:30 -0500

    Update LoanTable
    Co-Authored-By: tomminator97 <tomminator97@users.noreply.github.com>

--- database/src/main/resources/create/LoanTable.sql
+++ database/src/main/resources/create/LoanTable.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS Loan;
 CREATE TABLE Loan (
+ id INTEGER PRIMARY KEY AUTOINCREMENT
  FOREIGN KEY (id) REFERENCES Item(id),
- loanType varchar(255),
- loanDate varchar(255),
+ loanTime varchar(255),
+ checkoutDate varchar(255),
+ dueDate varchar(255)
  );
