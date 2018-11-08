e6491b2c1a648f99bc08e444f01b26cd83d6fbe2
Luca Fiorilli <luca30@live.ca>
2018-11-08 14:49:06 -0500

    BUG FIX: Password not encoded when registering

--- service/src/main/java/com/soen343/project/service/database/RecordDatabase.java
+++ service/src/main/java/com/soen343/project/service/database/RecordDatabase.java
@@ -7,6 +7,7 @@ import com.soen343.project.repository.entity.catalog.itemspec.ItemSpecification;
 import com.soen343.project.repository.entity.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
@@ -18,17 +19,21 @@ public class RecordDatabase {
 
     private final UserGateway userRepository;
     private final ItemGateway itemRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public RecordDatabase(UserGateway userRepository, ItemGateway itemRepository) {
+    public RecordDatabase(UserGateway userRepository, ItemGateway itemRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.itemRepository = itemRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     public List<User> register(User userToRegister) {
         List<User> registeredUsers = userRepository.findAll();
 
         if(userToRegister.isUniqueFrom(registeredUsers)) {
+            String password = userToRegister.getPassword();
+            userToRegister.setPassword(bCryptPasswordEncoder.encode(password));
             userRepository.save(userToRegister);
             return userRepository.findAll();
         } else {
