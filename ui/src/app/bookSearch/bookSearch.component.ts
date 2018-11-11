7b1e3f503e6f70159b996930ec5286169d18824f
JeffreyLi16 <jeffreyli16@hotmail.com>
2018-11-11 01:04:08 -0500

    Added search endpoint calls #68

--- ui/src/app/bookSearch/bookSearch.component.ts
+++ ui/src/app/bookSearch/bookSearch.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Book} from '../catalog/dto/book';
 
 @Component({
@@ -19,6 +19,34 @@ export class bookSearchComponent implements OnInit {
 
   getAllBooks(): void {
     this.http.get<Array<Book>>('http://localhost:8080/user/catalog/getAll/book', {withCredentials: true}).subscribe(response => {
+      this.bookList = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+  searchBooks(title: string,
+              author: string,
+              publisher: string,
+              pubDate: string,
+              language: string,
+              format: string,
+              isbn10: string,
+              isbn13: string) {
+
+    let body = JSON.stringify({
+      "title": title,
+      "author": author,
+      "publisher": publisher,
+      "pubDate": pubDate,
+      "language": language,
+      "format": format,
+      "isbn10": isbn10,
+      "isbn13": isbn13
+    })
+
+    let headers = new HttpHeaders({"Content-Type": "application/json"});
+    let options = {headers: headers, withCredentials: true};
+    this.http.post<Array<Book>>('http://localhost:8080/user/catalog/search/book', body, options).subscribe(response => {
       this.bookList = response;
     }, error => {
       console.log(error);
--- ui/src/app/musicSearch/musicSearch.component.ts
+++ ui/src/app/musicSearch/musicSearch.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Music} from "../catalog/dto/music";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-catalog',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class musicSearchComponent implements OnInit {
 
-  constructor() { }
+  musicList: Array<Music> = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getAllMusics();
   }
 
+  getAllMusics(): void {
+    this.http.get<Array<Music>>('http://localhost:8080/user/catalog/getAll/music', {withCredentials: true}).subscribe(response => {
+      this.musicList = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+  searchMusics(title: string,
+               artist: string,
+               type: string,
+               releaseDate: string,
+               label: string,
+               asin: string) {
+
+    let body = JSON.stringify({
+      "title": title,
+      "artist": artist,
+      "releaseDate": releaseDate,
+      "label": label,
+      "type": type,
+      "asin": asin
+    })
+
+    let headers = new HttpHeaders({"Content-Type": "application/json"});
+    let options = {headers: headers, withCredentials: true};
+    this.http.post<Array<Music>>('http://localhost:8080/user/catalog/search/music', body, options).subscribe(response => {
+      this.musicList = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
