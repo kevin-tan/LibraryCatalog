a0c3ffd1aa3844516c607b688361bf563ae943c8
Neqqash Hussin <neqqash96@gmail.com>
2018-11-14 00:08:58 -0500

    Create Loan Transaction Entity
    Loan Transaction Entity is a subclass of the Transaction Entity. It has an additional attribute; dueDate.
    
    Co-Authored-By: tomminator97 <tomminator97@users.noreply.github.com>

---
+++ repository/src/main/java/com/soen343/project/repository/entity/transaction/LoanTransaction.java
@@ -0,0 +1,42 @@
+package com.soen343.project.repository.entity.transaction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.soen343.project.repository.entity.catalog.item.Item;
+import com.soen343.project.repository.entity.user.Client;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import static com.soen343.project.repository.entity.EntityConstants.*;
+
+@Data
+@NoArgsConstructor
+public class LoanTransaction extends Transaction {
+
+    private Date dueDate;
+
+    @Builder
+    public LoanTransaction(Long id, Client client, Item item, String transactionType, Date checkoutDate, Date dueDate) {
+        super(id, client, item, transactionType, checkoutDate);
+        this.dueDate = dueDate;
+
+    }
+
+    @Override
+    @JsonIgnore
+    public String sqlUpdateValues() {
+        String columnValues = super.sqlUpdateValues() + (DUEDATE + " = '" + dueDate + "'");
+
+        return columnValues;
+
+    }
+
+    @Override
+    @JsonIgnore
+    public String toSQLValue() {
+        return "('" + super.toSQLValue() + dueDate + "')";
+    }
+
+}
