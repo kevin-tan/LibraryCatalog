85c5b540007b649c4596c472d7d53672e2d3370a
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2018-11-17 17:42:59 -0500

    Refactored code + Added URI for getting loaned items

--- endpoint/src/main/java/com/soen343/project/endpoint/controller/LoanController.java
+++ endpoint/src/main/java/com/soen343/project/endpoint/controller/LoanController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -50,6 +47,11 @@ public class LoanController {
     @PostMapping("/{clientId}/return")
     public ResponseEntity<?> returnItems(@PathVariable Long clientId, @RequestBody List<LoanableItem> loanableItems) {
         return new ResponseEntity<>(clientHandler.returnItems(clientId, loanableItems), HttpStatus.OK);
+    }
+
+    @GetMapping("/{clientId}/loanedItem")
+    public ResponseEntity<?> searchTransactionByAttribute(@PathVariable Long clientId) {
+        return new ResponseEntity<>(clientHandler.getLoanedItemsByUserID(clientId), HttpStatus.OK);
     }
 
 }
