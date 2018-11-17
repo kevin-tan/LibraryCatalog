210e1f769fb648745d6b50150b26598d25500c57
SagarPatel43 <patelsagar1997@gmail.com>
2018-11-16 19:19:55 -0500

    added angular material + sorting

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {HomeRedirectService} from './home-redirect.service';
 import {HttpClient} from '@angular/common/http';
 import {Book} from '../catalog/dto/book';
 import {Magazine} from '../catalog/dto/magazine';
 import {Movie} from '../catalog/dto/movie';
 import {Music} from '../catalog/dto/music';
+import {MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-home',
@@ -14,10 +15,14 @@ import {Music} from '../catalog/dto/music';
 
 export class HomeComponent implements OnInit {
 
+  displayBookColumns: string[] = ['title', 'author', 'pages', 'format', 'publisher', 'isbn10', 'isbn13', 'pubDate', 'language'];
+  matBookList: MatTableDataSource<Book>;
   bookList: Array<Book> = [];
   magazineList: Array<Magazine> = [];
   movieList: Array<Movie> = [];
   musicList: Array<Music> = [];
+
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
     //Gets all itemSpecs at beginning
@@ -53,6 +58,8 @@ export class HomeComponent implements OnInit {
   getAllCatalog() {
     this.http.get('http://localhost:8080/user/catalog/searchAll', {withCredentials: true}).subscribe(response => {
         this.bookList = response['book'] as Array<Book>;
+        this.matBookList = new MatTableDataSource(response['book'] as Array<Book>);
+        this.matBookList.sort = this.sort;
         this.magazineList = response['magazine'] as Array<Magazine>;
         this.movieList = response['movie'] as Array<Movie>;
         this.musicList = response['music'] as Array<Music>;
