750ff3aae72144a35dc529a4dcb7e231288532db
SagarPatel43 <patelsagar1997@gmail.com>
2018-11-17 23:51:29 -0500

    #27 fixed bug with adding movie specs

--- service/src/main/java/com/soen343/project/service/catalog/CatalogSession.java
+++ service/src/main/java/com/soen343/project/service/catalog/CatalogSession.java
@@ -2,12 +2,14 @@ package com.soen343.project.service.catalog;
 
 import com.soen343.project.database.query.QueryBuilder;
 import com.soen343.project.repository.concurrency.Scheduler;
+import com.soen343.project.repository.dao.catalog.itemspec.operation.ItemSpecificationOperation;
 import com.soen343.project.repository.entity.catalog.item.Item;
 import com.soen343.project.repository.entity.catalog.itemspec.ItemSpecification;
+import com.soen343.project.repository.entity.catalog.itemspec.media.Movie;
 import com.soen343.project.repository.uow.UnitOfWork;
 import lombok.Data;
 
-import static com.soen343.project.database.connection.DatabaseConnector.executeUpdate;
+import static com.soen343.project.repository.entity.EntityConstants.MOVIE_TABLE;
 
 @Data
 class CatalogSession {
@@ -28,13 +30,17 @@ class CatalogSession {
     }
 
     void addEntry(Item item) {
-        unitOfWork
-                .registerOperation(statement -> statement.executeUpdate(QueryBuilder.createSaveQuery(item.getTableWithColumns(), item.toSQLValue())));
+        unitOfWork.registerOperation(
+                statement -> statement.executeUpdate(QueryBuilder.createSaveQuery(item.getTableWithColumns(), item.toSQLValue())));
     }
 
     void addEntry(ItemSpecification itemSpec) {
-        unitOfWork
-                .registerOperation(statement -> statement.executeUpdate(QueryBuilder.createSaveQuery(itemSpec.getTableWithColumns(), itemSpec.toSQLValue())));
+        if (itemSpec.getTable().equals(MOVIE_TABLE)) {
+            unitOfWork.registerOperation(ItemSpecificationOperation.movieSaveOperation((Movie) itemSpec));
+        }else {
+            unitOfWork.registerOperation(statement -> statement.executeUpdate(
+                    QueryBuilder.createSaveQuery(itemSpec.getTableWithColumns(), itemSpec.toSQLValue())));
+        }
     }
 
     void removeEntry(Item item) {
@@ -42,7 +48,8 @@ class CatalogSession {
     }
 
     void removeEntry(ItemSpecification itemSpec) {
-        unitOfWork.registerOperation(statement -> statement.executeUpdate(QueryBuilder.createDeleteQuery(itemSpec.getTable(), itemSpec.getId())));
+        unitOfWork.registerOperation(
+                statement -> statement.executeUpdate(QueryBuilder.createDeleteQuery(itemSpec.getTable(), itemSpec.getId())));
     }
 
     void endSession() {
