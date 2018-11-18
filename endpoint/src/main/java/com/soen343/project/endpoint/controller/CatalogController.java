c923159aa8f2a7031fc0bcd8082481fbd8a17dd8
Kevin Tan <kevin.tan97@hotmail.com>
2018-11-18 10:30:45 -0500

    adding retrieve all per item type + itemspecid

--- endpoint/src/main/java/com/soen343/project/endpoint/controller/CatalogController.java
+++ endpoint/src/main/java/com/soen343/project/endpoint/controller/CatalogController.java
@@ -1,7 +1,6 @@
 package com.soen343.project.endpoint.controller;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.soen343.project.repository.entity.catalog.itemspec.ItemSpecification;
 import com.soen343.project.service.catalog.Catalog;
 import com.soen343.project.service.catalog.CatalogSearch;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +20,13 @@ public class CatalogController {
     private final CatalogSearch catalogSearch;
 
     @Autowired
-    public CatalogController(Catalog catalog, CatalogSearch catalogSearch){
+    public CatalogController(Catalog catalog, CatalogSearch catalogSearch) {
         this.catalog = catalog;
         this.catalogSearch = catalogSearch;
     }
 
     @GetMapping("/catalog")
-    public ResponseEntity<?> viewCatalogInventory(){
+    public ResponseEntity<?> viewCatalogInventory() {
         return new ResponseEntity<>(catalog.getAllItems(), HttpStatus.OK);
     }
 
@@ -39,9 +38,7 @@ public class CatalogController {
     @PostMapping("/catalog/search/{itemType}")
     public ResponseEntity<?> searchCatalogByAttribute(@PathVariable String itemType, @RequestBody ObjectNode objectNode) {
         Map<String, String> attributeValue = new HashMap<>();
-        objectNode.fieldNames().forEachRemaining(key ->
-                attributeValue.put(key,objectNode.get(key).asText())
-        );
+        objectNode.fieldNames().forEachRemaining(key -> attributeValue.put(key, objectNode.get(key).asText()));
 
         return new ResponseEntity<>(catalogSearch.searchCatalogByAttribute(itemType, attributeValue), HttpStatus.OK);
     }
@@ -54,5 +51,10 @@ public class CatalogController {
     @GetMapping("/catalog/getAll/{itemType}")
     public ResponseEntity<?> getAllOfType(@PathVariable String itemType) {
         return new ResponseEntity<>(catalogSearch.getAllOfType(itemType), HttpStatus.OK);
+    }
+
+    @GetMapping("/catalog/getAll/{itemType}/{itemSpecId}")
+    public ResponseEntity<?> getAllOfSameType(@PathVariable String itemType, @PathVariable Long itemSpecId) {
+        return new ResponseEntity<>(catalogSearch.getAllOfSameType(itemType, itemSpecId), HttpStatus.OK);
     }
 }
