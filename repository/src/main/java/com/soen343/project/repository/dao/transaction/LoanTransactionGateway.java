b1029300dea18ec1a1eb82b7c4530297fdc0b3f5
Kevin Tan <kevin.tan97@hotmail.com>
2018-11-17 19:04:24 -0500

    optimize import

--- repository/src/main/java/com/soen343/project/repository/dao/transaction/LoanTransactionGateway.java
+++ repository/src/main/java/com/soen343/project/repository/dao/transaction/LoanTransactionGateway.java
@@ -19,7 +19,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
