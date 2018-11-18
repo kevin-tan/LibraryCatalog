73487f7be138fa3711d6a91c3366ca3ea68aec49
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2018-11-17 19:40:29 -0500

    Added ui components

---
+++ ui/src/app/return/return.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {HomeRedirectService} from "../home/home-redirect.service";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-return',
+  templateUrl: './return.component.html',
+  styleUrls: ['./return.component.css']
+})
+export class ReturnComponent implements OnInit {
+
+  constructor(private http: HttpClient, private homeRedirectService: HomeRedirectService) { }
+
+  ngOnInit() {
+  }
+
+
+
+}
